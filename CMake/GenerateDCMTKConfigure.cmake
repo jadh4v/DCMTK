c1ca67427a23d756e2ded5e9e5f146ee475bac77
Shreeraj Jadhav <shreeraj.jadhav@kitware.com>
2022-08-10 17:02:50 -0400

    COMP: fix missing <csetjmp> header issue
    WASI compiler doesn't support C++ header csetjmp which is used
    in dcmtk and dcmtk/dcmjpeg for calling functions such as
    setjmp() and longjmp().
    
    Remove the dependency on csetjmp header.
    
    Related to the fix at jadh4v/itk-wasm@146801a

--- CMake/GenerateDCMTKConfigure.cmake
+++ CMake/GenerateDCMTKConfigure.cmake
@@ -284,7 +284,7 @@ endif()
   CHECK_INCLUDE_FILE_CXX("new.h" HAVE_NEW_H)
   CHECK_INCLUDE_FILE_CXX("pwd.h" HAVE_PWD_H)
   CHECK_INCLUDE_FILE_CXX("semaphore.h" HAVE_SEMAPHORE_H)
-  CHECK_INCLUDE_FILE_CXX("setjmp.h" HAVE_SETJMP_H)
+  # CHECK_INCLUDE_FILE_CXX("setjmp.h" HAVE_SETJMP_H)
   CHECK_INCLUDE_FILE_CXX("sstream" HAVE_SSTREAM)
   CHECK_INCLUDE_FILE_CXX("sstream.h" HAVE_SSTREAM_H)
   CHECK_INCLUDE_FILE_CXX("stat.h" HAVE_STAT_H)
@@ -720,7 +720,7 @@ endif()
   CHECK_FUNCTIONWITHHEADER_EXISTS("pclose" "${HEADERS}" HAVE_PCLOSE)
 
   # Signal handling functions
-  CHECK_FUNCTIONWITHHEADER_EXISTS("sigjmp_buf definition" "setjmp.h" HAVE_SIGJMP_BUF)
+  # CHECK_FUNCTIONWITHHEADER_EXISTS("sigjmp_buf definition" "setjmp.h" HAVE_SIGJMP_BUF)
 
 if(HAVE_LOCKF AND ANDROID)
   # When Android introduced lockf, they forgot to put the constants like F_LOCK in the
